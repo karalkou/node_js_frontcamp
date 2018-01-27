99e9d07e50ed3431fd4f5eb53a4a3bfd4f322ddd
Yury Karalkou <yury.karalkou@gmail.com>
2018-01-27 21:17:42 +0300

    [YK] Break into mongodb driver error

---
+++ app/routes/note_routes.js
@@ -0,0 +1,18 @@
+let ObjectID = require('mongodb').ObjectID;
+
+module.exports = function(app, db) {
+
+    app.post('/notes', (req, res) => {
+        const note = { text: req.body.body, title: req.body.title };
+
+        db.collection('notes').insert(note, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
+
+    });
+
+};
