dbe2ca88dd48b7467405951f4020c1b8f7dd1cf9
Yury Karalkou <yury.karalkou@gmail.com>
2018-01-28 12:07:20 +0300

    [YK] Add logger (morgan writes logs to console). Add template view rendering

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+const path        = require('path');
 const express     = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser  = require('body-parser');
-
+const logger      = require('morgan');
 const db          = require('./config/db');
 
-const app         = express();
-
+const app  = express();
 const port = 8000;
 
+// view engine setup
+app.set('views', path.join(__dirname, 'app/views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 MongoClient.connect(db.url, (err, database) => {
@@ -18,4 +23,24 @@ MongoClient.connect(db.url, (err, database) => {
     app.listen(port, () => {
         console.log('We are live on ' + port);
     });
+
+
+    // catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+        let err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
+
+    // error handler
+    app.use(function(err, req, res, next) {
+        // set locals, only providing error in development
+        res.locals.message = err.message;
+        res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+        // render the error page
+        res.status(err.status || 500);
+        res.render('error');
+    });
 });
+
