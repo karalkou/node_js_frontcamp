6303e859ed6a0911c356a33bda711b9ed18cff0f
Karalkou <yury.karalkou@gmail.com>
2018-02-03 11:23:50 +0300

    [YK] Add AuthController. Change routes in index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const auth = require("../controllers/AuthController.js");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Keep calm and be Expressed', greet: 'this wonderful world' });
-});
+
+// restrict index for logged in user only
+router.get('/', auth.home);
+
+// route to register page
+router.get('/register', auth.register);
+
+// route for register action
+router.post('/register', auth.doRegister);
+
+// route to login page
+router.get('/login', auth.login);
+
+// route for login action
+router.post('/login', auth.doLogin);
+
+// route for logout action
+router.get('/logout', auth.logout);
+
 
 module.exports = router;
+/*----------------------------------------------------*/
