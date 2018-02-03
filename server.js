efe830e07e224b8589abbb25a86a2f037109e584
Karalkou <yury.karalkou@gmail.com>
2018-02-03 11:23:09 +0300

    [YK] Configure express-session. Add passport configuration. Add views

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const path       = require('path');
-const fs         = require('fs');
-const express    = require('express');
-const bodyParser = require('body-parser');
-const logger     = require('morgan');
+const path         = require('path');
+const fs           = require('fs');
+const express      = require('express');
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const logger       = require('morgan');
 
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+const LocalStrategy = require('passport-local').Strategy; // Username and password authentication strategy for Passport and Node.js.
 
 const index = require('./app/routes/index');
 const blogs = require('./app/routes/blogs_routes');
@@ -22,6 +23,14 @@ let accessLogStream = fs.createWriteStream(path.join(__dirname, 'app/access.log'
 app.use(logger('combined', {stream: accessLogStream}));
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.listen(port, () => {
     console.log('We are live on ' + port);
@@ -31,6 +40,11 @@ app.listen(port, () => {
 app.use('/', index);
 app.use('/blogs', blogs);
 
+// passport configuration
+const User = require('./app/models/User');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -53,7 +67,9 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
     const errStatus = err.status;
     res.locals.message = err.message;
-    res.locals.error = {status: errStatus};
+    res.locals.error = {
+        status: errStatus
+    };
 
     // render the error page
     res.status(err.status || 500);
