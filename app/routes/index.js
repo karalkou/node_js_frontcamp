b5d8733b93e9ea846435705c69eba294d4beb405
Karalkou <yury.karalkou@gmail.com>
2018-02-03 07:24:57 +0300

    [YK] Change a bit index.pug template

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Keep calm and be Expressed' });
+    res.render('index', { title: 'Keep calm and be Expressed', greet: 'this wonderful world' });
 });
 
 module.exports = router;
